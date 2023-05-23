8758891fe7ab53da96d82285e154e58889c77512
charles <ketchasso72@gmail.com>
2023-05-23 23:06:03 +0100

    feed section

---
+++ src/atoms/Feed.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+function Feed() {
+  return (
+    <div className="xl:ml-[500px] border-l border-r xl:min-w-[576px] sm:ml-[73px]">
+      title
+    </div>
+  );
+};
+
+export default Feed;
